89bca9b4a8d46808799864c22d878831425e4faf
Varun114x <thampivarun114@gmail.com>
2022-07-02 14:22:48 +0530

    angularcompleted

---
+++ src/app/facuilty/facuilty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-facuilty',
+  templateUrl: './facuilty.component.html',
+  styleUrls: ['./facuilty.component.css']
+})
+export class FacuiltyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
